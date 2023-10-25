55f1ed9a51a35e47868b3794327335cc84074bae
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2023-10-25 15:07:31 -0300

    Ignite festas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <nav id="navbar">
-      <h2>Party Time</h2>
+      <h2>Ignite Festas</h2>
       <ul>
         <li>
           <NavLink to="/">Minhas Festas</NavLink>
